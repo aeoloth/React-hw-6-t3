b1ed7b3fcd3919da58a3e0391a7b26a2a2fe1bc3
Eldar Zainashev <aeoloth@yandex.ru>
2021-09-10 21:24:30 +0300

    3

--- hw-6-t3/src/App.js
+++ hw-6-t3/src/App.js
@@ -1,25 +1,98 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const ErrorComponent = () => <div>{props.ignore}</div>
+
+export default class Counter extends React.Component {
+  constructor(props) {
+    console.log("constructor");
+    super(props);
+
+    this.state = {
+      counter: 0,
+      seed: 0,
+      initializing: true
+    };
+
+    this.increment = () => this.setState({ counter: this.state.counter + 1 });
+    this.decrement = () => this.setState({ counter: this.state.counter - 1 });
+  }
+
+  static getDerivedStateFromProps(props, state) {
+    if (props.seed && state.seed !== props.seed) {
+      return {
+        seed: props.seed,
+        counter: props.seed,
+      }; 
+    }
+
+    return null;
+  }
+
+  componentDidMount() {
+    console.log("Component did mount");
+    setTimeout(() => {
+      this.setState({initializing: false})
+    }, 500)
+    console.log("-------------------");
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    if (
+      nextProps.ignoreProp &&
+      this.props.ignoreProp !== nextProps.ignoreProp
+    ) {
+      console.log("Should component update - DO NOT RENDER");
+      console.log("-------------------");
+      return false;
+    }
+    console.log("Should component update - RENDER");
+    return true;
+  }
+
+  getSnapshotBeforeUpdate(prevProps, prevState) {
+    console.log("Get snapshot before update");
+    return null;
+  }
+
+  render() {
+    console.log("Render", this.state.error);
+
+    if(this.state.initializing) {
+      return <div>Initializing...</div>
+    }
+
+    if (this.props.showErrorComponent && this.state.error) {
+      return <div>We have encountered an error {this.state.error.message}</div>
+    }
+
+    return (
+      <div>
+        <button onClick={this.increment}>Increment</button>
+        <button onClick={this.decrement}>Decrement</button>
+        <div className="counter">
+          Counter: {this.state.counter}
+        </div>
+        {this.props.showErrorComponent ? <ErrorComponent /> : null}
+      </div>
+    );
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    console.log("Component did update");
+    console.log("-------------------");
+  }
+
+  componentWillUnmount() {
+    console.log("Component will unmount");
+    console.log("-------------------");
+  }
+
+  componentDidCatch(error, info) {
+    console.log("Component did catch")
+
+    this.setState({ error, info })
+  }
 }
 
-export default App;
+
--- hw-6-t3/src/index.js
+++ hw-6-t3/src/index.js
@@ -1,17 +1,50 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Counter from './App';
+
+class App extends React.Component {
+constructor(props) {
+  super(props);
+
+  this.state  ={
+    mount: true,
+    ignoreProp: 0,
+    seed: 40,
+    showErrorComponent: false
+  }
+
+  this.mountCounter = () =>  this.setState({mount: true});
+  this.unmountCounter = () => this.setState({mount: false});
+
+  this.ignoreProp = () => this.setState({ignoreProp: Math.random()});
+  this.seedGenerator = () => this.setState({seed: Number.parseInt(Math.random()*100)})
+  this.toggleError = () => this.setState({showErrorComponent: !this.state.showErrorComponent})
+}
+
+  render() {
+    return(
+      <div>
+        <button onClick={this.mountCounter} disabled={this.state.mount}>Mount Counter</button>
+        <button onClick={this.unmountCounter} disabled={!this.state.mount}>Unmount Counter</button>
+        <button onClick={this.ignoreProp} >Ignore Prop</button>
+        <button onClick={this.seedGenerator} >Generate Seed</button>
+        <button onClick={this.toggleError} >Toggle Error</button>
+        {this.state.mount ? 
+        <Counter 
+        ignoreProp={this.state.ignoreProp}
+        seed={this.state.seed}
+        showErrorComponent={this.state.showErrorComponent}
+         /> : 
+        null}
+      </div>
+    )
+  }
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
